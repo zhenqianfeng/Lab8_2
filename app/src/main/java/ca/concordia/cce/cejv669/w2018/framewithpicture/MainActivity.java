186f55b4633f4e3c6e15a23bc1c09deeecfbe001
zhenqianfeng <ferretfeng@yahoo.ca>
2019-02-14 21:37:14 -0500

    Initial commit

---
+++ app/src/main/java/ca/concordia/cce/cejv669/w2018/framewithpicture/MainActivity.java
@@ -0,0 +1,13 @@
+package ca.concordia.cce.cejv669.w2018.framewithpicture;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/test/java/ca/concordia/cce/cejv669/w2018/framewithpicture/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package ca.concordia.cce.cejv669.w2018.framewithpicture;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
